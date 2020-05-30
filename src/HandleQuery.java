0bc4f98ca3cc8b80c9285f02a371c8beeb1f9300
Ali Magde <galimagde10@gmail.com>
2020-05-30 12:01:05 +0200

    Updated Handle Query class added data files

--- src/HandleQuery.java
+++ src/HandleQuery.java
@@ -14,7 +14,13 @@ public class HandleQuery {
     void queryA(ArrayList<String> arr, Socket s, String server) {
         try {
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.writeUTF("Found site type A");
+            if (server.equals("Auth server")) {
+                out.writeUTF("Url = " + arr.get(0) + " IP Address = " + arr.get(1) + " Query type = A ,NS " + " Server name = " + server);
+                return;
+            } else {
+                out.writeUTF("Url = " + arr.get(0) + " IP Address = " + arr.get(1) + " Query type = A" + " Server name = " + server);
+                return;
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -23,7 +29,7 @@ public class HandleQuery {
     void queryC(ArrayList<String> arr, Socket s, String server) {
         try {
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.writeUTF("Found site type C");
+            out.writeUTF("URL = "+arr.get(0)+" IP Address = "+arr.get(1)+" Query type = A,CNAME "+"Server name:" +server + " Canonical name: "+arr.get(2)+" Aliases:"+arr.get(0));
         } catch (Exception e) {
             System.out.println(e);
         }
